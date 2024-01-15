c7e184aa02d0cd534ff46e71478ab3bd7cf10744
Fikret <fikretonurozdil@gmail.com>
2024-01-15 16:59:21 +0300

    Initial Commit

---
+++ GutenbergReader/GutenbergReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GutenbergReaderApp.swift
+//  GutenbergReader
+//
+//  Created by Fikret Onur ÖZDİL on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GutenbergReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
