11f5513458abe2669529958b7a3242d0b5c2dd3f
Fikret <fikretonurozdil@gmail.com>
2024-01-21 11:56:42 +0300

    Update AppFeatureView.swift

--- GutenbergReader/Features/App/AppFeatureView.swift
+++ GutenbergReader/Features/App/AppFeatureView.swift
@@ -10,54 +10,68 @@ import ComposableArchitecture
 
 struct AppFeatureView: View {
     let store: StoreOf<AppFeature>
+    fileprivate func HomeView(_ viewStore: ViewStore<AppFeature.State, AppFeature.Action>) -> VStack<TupleView<(HStack<TupleView<(Spacer, Button<some View>, Spacer, Button<some View>, Spacer)>>, some View)>> {
+        return VStack(alignment: .center) {
+            HStack {
+                Spacer()
+                Button {
+                    viewStore.send(.changeTab(.recentlyAdded))
+                } label: {
+                    Text("Recently Added")
+                        .frame(width: 150, height: 50)
+                        .background {
+                            Rectangle()
+                                .stroke(lineWidth: 1.0)
+                        }
+                }
+                Spacer()
+                
+                Button {
+                    viewStore.send(.changeTab(.bookmarks))
+                } label: {
+                    Text("Bookmarks")
+                        .frame(width: 150, height: 50)
+                        .background {
+                            Rectangle()
+                                .stroke(lineWidth: 1.0)
+                        }
+                }
+                
+                Spacer()
+            }
+            
+            TabView(selection: viewStore.binding(get: \.appTab, send: { .changeTab($0) } )) {
+                BooksListView(store: store.scope(state: \.recentlyAddedTab, action: \.recentlyAddedTab))
+                    .tag(AppTab.recentlyAdded)
+                    .onAppear {
+                        viewStore.send(.recentlyAddedTab(.onAppear))
+                    }
+                BooksListView(store: store.scope(state: \.bookmarksTab, action: \.bookmarksTab))
+                    .tag(AppTab.bookmarks)
+                    .onAppear {
+                        viewStore.send(.bookmarksTab(.onAppear))
+                    }
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .onAppear {
+                viewStore.send(.onAppear)
+            }
+        }
+    }
+    
     var body: some View {
         WithViewStore(self.store) { $0 } content: { viewStore in
-            VStack(alignment: .center) {
-                HStack {
-                    Spacer()
-                    Button {
-                        viewStore.send(.changeTab(.recentlyAdded))
-                    } label: {
-                        Text("Recently Added")
-                            .frame(width: 150, height: 50)
-                            .background {
-                                Rectangle()
-                                    .stroke(lineWidth: 1.0)
-                            }
+            TabView {
+                HomeView(viewStore)
+                    .tabItem {
+                        Label("Home", systemImage: "house")
                     }
-                    Spacer()
-
-                    Button {
-                        viewStore.send(.changeTab(.bookmarks))
-                    } label: {
-                        Text("Bookmarks")
-                            .frame(width: 150, height: 50)
-                            .background {
-                                Rectangle()
-                                    .stroke(lineWidth: 1.0)
-                            }
+                SearchFeatureView(store: self.store.scope(state: \.searchTab, action: \.searchTab))
+                    .tabItem {
+                        Label("Search", systemImage: "magnifyingglass")
                     }
-
-                    Spacer()
-                }
-
-                TabView(selection: viewStore.binding(get: \.appTab, send: { .changeTab($0) } )) {
-                    BooksListView(store: store.scope(state: \.recentlyAddedTab, action: \.recentlyAddedTab))
-                        .tag(AppTab.recentlyAdded)
-                        .onAppear {
-                            viewStore.send(.recentlyAddedTab(.onAppear))
-                        }
-                    BooksListView(store: store.scope(state: \.bookmarksTab, action: \.bookmarksTab))
-                        .tag(AppTab.bookmarks)
-                        .onAppear {
-                            viewStore.send(.bookmarksTab(.onAppear))
-                        }
-                }
-                .tabViewStyle(.page(indexDisplayMode: .never))
-                .onAppear {
-                    viewStore.send(.onAppear)
-                }
             }
+
         }
     }
 }
@@ -65,4 +79,3 @@ struct AppFeatureView: View {
  #Preview {
      AppFeatureView(store: GutenbergReaderApp.store)
  }
-
